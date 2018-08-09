71ab018b2c402b82a53fdcc4591172d941312a75
Yasset Perez-Riverol <ypriverol@gmail.com>
2018-08-09 14:07:53 +0100

    minor changes

--- src/main/java/uk/ac/ebi/pride/ws/pride/controllers/StatsController.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/controllers/StatsController.java
@@ -16,12 +16,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import uk.ac.ebi.pride.archive.dataprovider.utils.Tuple;
+import uk.ac.ebi.pride.mongodb.archive.model.stats.MongoPrideStats;
 import uk.ac.ebi.pride.mongodb.archive.service.stats.PrideStatsMongoService;
 import uk.ac.ebi.pride.ws.pride.hateoas.CustomPagedResourcesAssembler;
 import uk.ac.ebi.pride.ws.pride.utils.APIError;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -61,9 +65,32 @@ public class StatsController {
     @RequestMapping(value = "/stats/{name}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Object> statistics(@PathVariable(value = "name", required = true, name = "name") String name){
 
-        List<Tuple<String, Integer>> stats = mongoStatsService.findLastGeneratedStats().getSubmissionsCount().get(name);
+        Object stats = mongoStatsService.findLastGeneratedStats().getSubmissionsCount().get(name);
+        if (stats == null || ((List)stats).size() == 0)
+            stats = mongoStatsService.findLastGeneratedStats().getComplexStats().get(name);
 
         return new ResponseEntity<>(stats, HttpStatus.ACCEPTED);
+    }
+
+
+    @ApiOperation(notes = "Retrieve all statistics keys and names", value = "statistics", nickname = "getStatNames", tags = {"stats"} )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK", response = APIError.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = APIError.class)
+    })
+    @RequestMapping(value = "/stats/", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Object> getStatisticsNames(){
+
+        List<String> statNames = new ArrayList<>();
+        MongoPrideStats stats = mongoStatsService.findLastGeneratedStats();
+        if (stats != null){
+            if(stats.getSubmissionsCount() != null)
+                statNames.addAll(stats.getSubmissionsCount().entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toList()));
+            if(stats.getComplexStats() != null)
+                statNames.addAll(stats.getComplexStats().entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toList()));
+        }
+
+        return new ResponseEntity<>(statNames, HttpStatus.ACCEPTED);
     }
 
 }
