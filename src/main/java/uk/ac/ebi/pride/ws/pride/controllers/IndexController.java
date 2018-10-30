fbe245655b7d345d97908c6f33c3478704d22891
Avinash Inuganti <inuganti@ebi.ac.uk>
2018-10-30 16:26:23 +0000

    aap and security integration

--- src/main/java/uk/ac/ebi/pride/ws/pride/controllers/IndexController.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/controllers/IndexController.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.pride.ws.pride.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.View;
 import springfox.documentation.annotations.ApiIgnore;
 
