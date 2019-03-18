96f0f7792bd606f78ac749a1d2d557d48db57ed4
avinash <inuganti@ebi.ac.uk>
2019-03-18 11:12:22 +0000

    Made header changes to enable CORS across other browsers. Tested for Chrome, Firefox and Safari

--- src/main/java/uk/ac/ebi/pride/ws/pride/security/config/SimpleCORSFilter.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/security/config/SimpleCORSFilter.java
@@ -14,6 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -33,14 +34,28 @@ public class SimpleCORSFilter extends OncePerRequestFilter {
             // CORS "pre-flight" request
             response.addHeader("Access-Control-Allow-Origin", "*");
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            response.addHeader("Access-Control-Allow-Headers", "*");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers");
             /*response.addHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,"
                     + "Access-Control-Request-Method,Access-Control-Request-Headers");*/
             /*response.addHeader("cors.support.credentials", "true");
             response.addHeader("cors.exposed.headers", "Access-Control-Allow-Origin,"
                     + "Access-Control-Allow-Credentials");*/
         }
-        System.out.println("########### Simple CORS Filter ############");
+        System.out.println("########### Simple CORS Filter: "+request.getRequestURL()+" ############");
+
+        /*filterChain.doFilter(new HttpServletRequestWrapper((HttpServletRequest) request) {
+            @Override
+            public String getRequestURI() {
+                // return what you want
+                String requestURL = request.getRequestURL().toString();
+                if(requestURL.contains("/v2")){
+                    return requestURL.replace("/v2","");
+                }else{
+                    return requestURL;
+                }
+            }
+        }, response);*/
+
         filterChain.doFilter(request, response);
     }
 }
