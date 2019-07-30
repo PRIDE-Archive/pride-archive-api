9ccd4c198fa21057782711f61753e18c65d36ac9
avinash <inuganti@ebi.ac.uk>
2019-07-30 11:48:22 +0100

    added more testing

--- src/test/java/uk/ac/ebi/pride/ws/test/integration/ArchiveAPITest.java
+++ src/test/java/uk/ac/ebi/pride/ws/test/integration/ArchiveAPITest.java
@@ -23,8 +23,16 @@ import uk.ac.ebi.pride.ws.pride.Application;
 import uk.ac.ebi.pride.ws.pride.configs.SolrCloudConfig;
 import uk.ac.ebi.pride.ws.pride.configs.SwaggerConfig;
 import uk.ac.ebi.pride.ws.pride.service.user.AAPService;
+import uk.ac.ebi.pride.ws.test.integration.service.TestService;
 import uk.ac.ebi.pride.ws.test.integration.util.DocumentationUtils;
+import uk.ac.ebi.pride.ws.test.integration.util.TestConstants;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -38,7 +46,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 
 @EnableAutoConfiguration
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class, ArchiveMongoConfig.class, SolrCloudConfig.class,SwaggerConfig.class})
+@SpringBootTest(classes = {Application.class, ArchiveMongoConfig.class, SolrCloudConfig.class,SwaggerConfig.class,TestService.class})
 @TestPropertySource(locations = "classpath:application.properties")
 @AutoConfigureRestDocs
 public class ArchiveAPITest {
@@ -57,6 +65,11 @@ public class ArchiveAPITest {
     @Autowired
     private  AAPService aapService;
 
+    @Autowired
+    private TestService testService;
+
+    private Map<String,String> testValuesMap;
+
     @Before
     public void setUp() {
 
@@ -72,13 +85,26 @@ public class ArchiveAPITest {
                         .withHost(host)
                         .withPort(80))
                 .build();
+
+        /*Populate required values for testing endpoints*/
+        testValuesMap = new HashMap<String,String>();
+
+        String fileAccession = testService.getFileAccession();
+        testValuesMap.put(TestConstants.FILE_ACCESSION,fileAccession);
+
+        testValuesMap.put(TestConstants.PROJECT_ACCESSION,"PRD000001");
+
+        List<String> msRunValuesList = testService.getMsRunFileAccession();
+        testValuesMap.put(TestConstants.FILE_ACCESSION_WITH_MSRUN, msRunValuesList.get(0));
+        testValuesMap.put(TestConstants.PROJECT_ACCESSION_WITH_MSRUN,msRunValuesList.get(1));
     }
 
     /*Files Tests*/
     @Test
     public void getFileTest() throws Exception {
 
-        this.mockMvc.perform(get("/files/{fileAccession}","PXF00001876616").accept(MediaType.APPLICATION_JSON))
+
+        this.mockMvc.perform(get("/files/{fileAccession}",testValuesMap.get(TestConstants.FILE_ACCESSION)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-file", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(
@@ -87,11 +113,11 @@ public class ArchiveAPITest {
 
     @Test
     public void getAllFilesTest() throws Exception {
-        this.mockMvc.perform(get("/files?filter=accession==PXF00001535821&pageSize=5&page=0&sortDirection=DESC&sortConditions=submissionDate").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/files?filter=accession=="+testValuesMap.get(TestConstants.FILE_ACCESSION)+"&pageSize=5&page=0&sortDirection=DESC&sortConditions=submissionDate").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-all-files", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), requestParameters(
-                        parameterWithName("filter").description("Parameters to filter the search results. The strcuture of the filter is: field1==value1, field2==value2. Example accession==PXF00001535821. This filter allows advance querying and more information can be found at link:#_advance_filter[Advance Filter]"),
+                        parameterWithName("filter").description("Parameters to filter the search results. The structure of the filter is: field1==value1, field2==value2. Example accession=="+testValuesMap.get(TestConstants.FILE_ACCESSION)+". This filter allows advance querying and more information can be found at link:#_advance_filter[Advance Filter]"),
                         parameterWithName("pageSize").description("Number of results to fetch in a page"),
                         parameterWithName("page").description("Identifies which page of results to fetch"),
                         parameterWithName("sortDirection").description("Sorting direction: ASC or DESC"),
@@ -115,7 +141,7 @@ public class ArchiveAPITest {
     @Test
     public void getProjectTest() throws Exception {
 
-        this.mockMvc.perform(get("/projects/{accession}","PRD000001").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/projects/{accession}",testValuesMap.get(TestConstants.PROJECT_ACCESSION)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-project", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(
@@ -125,11 +151,11 @@ public class ArchiveAPITest {
     @Test
     public void getProjectFilesTest() throws Exception {
 
-        this.mockMvc.perform(get("/projects/{accession}/files?filter=fileName==PRIDE_Exp_Complete_Ac_1.pride.mgf.gz&pageSize=5&page=0&sortDirection=DESC&sortConditions=submissionDate","PRD000001").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/projects/{accession}/files?filter=fileName==PRIDE_Exp_Complete_Ac_1.pride.mgf.gz&pageSize=5&page=0&sortDirection=DESC&sortConditions=submissionDate",testValuesMap.get(TestConstants.PROJECT_ACCESSION)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-project-files", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(parameterWithName("accession").description("The Accession id associated with this project")),requestParameters(
-                                parameterWithName("filter").description("Parameters to filter the search results. The strcuture of the filter is: field1==value1, field2==value2. Example `fileName==PRIDE_Exp_Complete_Ac_1.pride.mgf.gz`. This filter allows advance querying and more information can be found at link:#_advance_filter[Advance Filter]"),
+                                parameterWithName("filter").description("Parameters to filter the search results. The structure of the filter is: field1==value1, field2==value2. Example `fileName==PRIDE_Exp_Complete_Ac_1.pride.mgf.gz`. This filter allows advance querying and more information can be found at link:#_advance_filter[Advance Filter]"),
                                 parameterWithName("pageSize").description("Number of results to fetch in a page"),
                                 parameterWithName("page").description("Identifies which page of results to fetch"),
                                 parameterWithName("sortDirection").description("Sorting direction: ASC or DESC"),
@@ -139,7 +165,7 @@ public class ArchiveAPITest {
     @Test
     public void getSimilarProjectsTest() throws Exception {
 
-        this.mockMvc.perform(get("/projects/{accession}/similarProjects?pageSize=5&page=0","PRD000001").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/projects/{accession}/similarProjects?pageSize=5&page=0",testValuesMap.get(TestConstants.PROJECT_ACCESSION)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-similar-project", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(parameterWithName("accession").description("The Accession id associated with this project")),requestParameters(
@@ -155,7 +181,7 @@ public class ArchiveAPITest {
                 .andDo(document("get-project-search", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), requestParameters(
                                 parameterWithName("keyword").description("The entered word will be searched among the fields to fetch matching projects."),
-                                parameterWithName("filter").description("Parameters to filter the search results. The strcuture of the filter is: field1==value1, field2==value2. Example accession==PRD000001. More information on this can be found at link:#_filter[Filter]"),
+                                parameterWithName("filter").description("Parameters to filter the search results. The structure of the filter is: field1==value1, field2==value2. Example accession=="+testValuesMap.get(TestConstants.PROJECT_ACCESSION)+". More information on this can be found at link:#_filter[Filter]"),
                                 parameterWithName("pageSize").description("Number of results to fetch in a page"),
                                 parameterWithName("page").description("Identifies which page of results to fetch"),
                                 parameterWithName("dateGap").description("A date range field with possible values of +1MONTH, +1YEAR"),
@@ -171,7 +197,7 @@ public class ArchiveAPITest {
                 .andDo(document("get-project-facet", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), requestParameters(
                                 parameterWithName("keyword").description("The entered word will be searched among the fields to fetch matching projects."),
-                                parameterWithName("filter").description("Parameters to filter the search results. The strcuture of the filter is: field1==value1, field2==value2. Example accession==PRD000001. More information on this can be found at link:#_filter[Filter]"),
+                                parameterWithName("filter").description("Parameters to filter the search results. The structure of the filter is: field1==value1, field2==value2. Example accession=="+testValuesMap.get(TestConstants.PROJECT_ACCESSION)+". More information on this can be found at link:#_filter[Filter]"),
                                 parameterWithName("facetPageSize").description("Number of results to fetch in a page"),
                                 parameterWithName("facetPage").description("Identifies which page of results to fetch"),
                                 parameterWithName("dateGap").description("A date range field with possible values of +1MONTH, +1YEAR"))));
@@ -187,7 +213,7 @@ public class ArchiveAPITest {
                         preprocessResponse(prettyPrint())));
     }
 
-    @Test
+    /*@Test
     public void getStatTest() throws Exception {
 
         this.mockMvc.perform(get("/stats/{name}","SUBMISSIONS_PER_MONTH").accept(MediaType.APPLICATION_JSON))
@@ -195,13 +221,13 @@ public class ArchiveAPITest {
                 .andDo(document("get-stat", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(
                                 parameterWithName("name").description("The name of the statistic to be returned."))));
-    }
+    }*/
 
     /*MSRun Tests*/
 
     @Test
     public void getMsRunsByProject() throws Exception{
-        this.mockMvc.perform(get("/msruns/byProject?accession=PXD005011").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/msruns/byProject?accession="+testValuesMap.get(TestConstants.PROJECT_ACCESSION_WITH_MSRUN)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("get-msrun-by-project", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), requestParameters(
@@ -210,7 +236,7 @@ public class ArchiveAPITest {
 
     @Test
     public void getMsRunsByFile() throws Exception{
-        this.mockMvc.perform(get("/msruns/{accession}","PXF00001876616").accept(MediaType.APPLICATION_JSON))
+        this.mockMvc.perform(get("/msruns/{accession}",testValuesMap.get(TestConstants.FILE_ACCESSION_WITH_MSRUN)).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is2xxSuccessful())
                 .andDo(document("get-msrun-by-file", preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()), pathParameters(
@@ -221,7 +247,7 @@ public class ArchiveAPITest {
     public void putMsRunsData() throws Exception{
         String aapToken = aapService.getAAPToken();
         String payload = "{\"MSRunMetadata\":{  \"additionalAttributesStrings\": [],  \"FileProperties\": [],  \"IdSettings\": [    {      \"id\": \"Protocol_1\",      \"FixedModifications\": [        {          \"massDelta\":57.021464,          \"residues\":[\"C\"],          \"composition\":\"H(3)C(2)NO\",          \"position\":\"Anywhere\",          \"name\":{            \"accession\":\"UNIMOD:4\",            \"name\":\"Carbamidomethyl\",            \"cvLabel\":\"UNIMOD\"          }        }      ],      \"VariableModifications\": [        {          \"massDelta\":0.984016,          \"residues\":[\"N\", \"Q\"],          \"position\":\"Anywhere\",          \"composition\":\"H(-1)N(-1)O\",          \"name\":{            \"accession\":\"UNIMOD:7\",            \"name\":\"Deamidated\",            \"cvLabel\":\"UNIMOD\"          }        },        {          \"massDelta\":15.994915,          \"residues\":[\"M\"],          \"position\":\"Anywhere\",          \"composition\":\"O\",          \"name\":{            \"accession\":\"UNIMOD:35\",            \"name\":\"Oxidation\",            \"cvLabel\":\"UNIMOD\"          }        }      ],      \"Enzymes\":[        {          \"id\":\"ENZ_0\",          \"cTermGain\":\"OH\",          \"nTermGain\":\"H\",          \"missedCleavages\":2,          \"semiSpecific\":\"0\",          \"SiteRegexp\":\"![CDATA[(?=[KR])(?!P)]]\",          \"name\":          {            \"accession\":\"MS:1001251\",            \"name\":\"Trypsin\",            \"cvLabel\":\"MS\"          }        }      ],      \"FragmentTolerance\":[        {          \"tolerance\":{            \"accession\":\"MS:1001413\",            \"name\":\"search tolerance minus value\",            \"value\":\"0.6\",            \"cvLabel\":\"MS\"          },          \"unit\":{            \"accession\":\"UO:0000221\",            \"name\":\"dalton\",            \"cvLabel\": \"UO\"          }        }      ],      \"ParentTolerance\":[        {          \"tolerance\":{            \"accession\":\"MS:1001412\",            \"name\":\"search tolerance plus value\",            \"value\":\"20\",            \"cvLabel\":\"MS\"          },          \"unit\":{            \"accession\":\"UO:0000169\",            \"name\":\"parts per million\",            \"cvLabel\": \"UO\"          }        },        {          \"tolerance\":{            \"accession\":\"MS:1001413\",            \"name\":\"search tolerance minus value\",            \"value\":\"20\",            \"cvLabel\":\"MS\"          },          \"unit\":{            \"accession\":\"UO:0000169\",            \"name\":\"parts per million\",            \"cvLabel\": \"UO\"          }        }      ]    }  ],  \"InstrumentProperties\": [    {      \"accession\": \"MS:1000494\",      \"name\": \"Thermo Scientific instrument model\",      \"value\": \"LTQ Orbitrap Velos\",      \"cvLabel\": \"MS\"    }  ],  \"MsData\": [],  \"ScanSettings\": []}}";
-            this.mockMvc.perform(put("/msruns/{accession}/updateMetadata","PXF00001876616")
+            this.mockMvc.perform(put("/msruns/{accession}/updateMetadata",testValuesMap.get(TestConstants.FILE_ACCESSION_WITH_MSRUN)/*"PXF00001876616"*/)
                 .header("Authorization", "Bearer "+aapToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON).content(payload))
                 .andExpect(status().isOk())
@@ -229,7 +255,7 @@ public class ArchiveAPITest {
                         preprocessResponse(prettyPrint()),
                         requestHeaders(
                                 headerWithName("Authorization").description(
-                                        "Access token beased authorization mechanism. Refer to authorization section for more information on obtaining the token"))/*,
+                                        "Access token beased authorization mechanism. Refer to authorization section for more information on obtaining the token")),
                         pathParameters(parameterWithName("accession").description("The unique file identifier.")),
                         requestFields(
                                 fieldWithPath("MSRunMetadata").description("The JSON payload containing the MSRunMetadata similar to the payload in example request."),
@@ -238,11 +264,11 @@ public class ArchiveAPITest {
                                 fieldWithPath("MSRunMetadata.InstrumentProperties").description("The JSON object containing the InstrumentProperties for raw files."),
                                 fieldWithPath("MSRunMetadata.ScanSettings").description("The JSON object containing the ScanSettings for raw files."),
                                 fieldWithPath("MSRunMetadata.MsData").description("The JSON object containing the MsData for raw files.")
-                        )*/));
+                        )));
     }
 
 
-    @Test
+    /*@Test
     public void putMsRunsDataPart() throws Exception{
         String aapToken = aapService.getAAPToken();
         String payload = "{\"MSRunMetadata\":{     \"FileProperties\": [       {         \"accession\": \"NCIT:C47922\",         \"cvLabel\": \"NCIT\",         \"name\": \"Pathname\",         \"value\": \"/Users/yperez/Downloads/SMXL7_GR24_1.raw\"       },       {         \"accession\": \"NCIT:C25714\",         \"cvLabel\": \"NCIT\",         \"name\": \"Version\",         \"value\": \"64\"       },       {         \"accession\": \"NCIT:C69199\",         \"cvLabel\": \"NCIT\",         \"name\": \"Content Creation Date\",         \"value\": \"15/07/2014 23:58:41\"       },       {         \"accession\": \"NCIT:C25365\",         \"cvLabel\": \"NCIT\",         \"name\": \"Description\",         \"value\": \"\"       }     ]   }   }";
@@ -257,7 +283,7 @@ public class ArchiveAPITest {
                                 headerWithName("Authorization").description(
                                         "Access token beased authorization mechanism. Refer to authorization section for more information on obtaining the token")),
                         requestParameters(parameterWithName("fieldName").description("The field inside MSRunMetadata to be updated. This can be any one of these: "+PrideArchiveField.MS_RUN_ID_SETTINGS+","+PrideArchiveField.MS_RUN_SCAN_SETTINGS+","+PrideArchiveField.MS_RUN_FILE_PROPERTIES+","+PrideArchiveField.MS_RUN_INSTRUMENT_PROPERTIES+","+PrideArchiveField.MS_RUN_MS_DATA+"."))));
-    }
+    }*/
 
 
     /*User Pages Tests*/
