f40b978ebdb35aeafc26280c69477b9bf9bc1104
yperez <ypriverol@gmail.com>
2020-05-31 16:29:20 +0100

    fix error with new line.

--- src/main/java/uk/ac/ebi/pride/ws/pride/utils/FileUtils.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/utils/FileUtils.java
@@ -2,11 +2,8 @@ package uk.ac.ebi.pride.ws.pride.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
 
 public class FileUtils {
 
@@ -18,7 +15,7 @@ public class FileUtils {
         StringBuffer fileString = new StringBuffer();
         String line;
         while ((line = read.readLine()) != null)
-            fileString.append(line);
+            fileString.append(line).append("\n");
         read.close();
         return fileString.toString();
 
