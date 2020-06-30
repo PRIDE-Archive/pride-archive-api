8aed00695e49999ef7653d71a7a02e835b4e2132
Chakradhar Bandla <chakrabandla@gmail.com>
2020-06-30 12:38:56 +0100

    minor

--- src/main/java/uk/ac/ebi/pride/ws/pride/configs/RepoConfig.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/configs/RepoConfig.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.pride.ws.pride.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,25 +9,16 @@ import uk.ac.ebi.pride.archive.repo.client.ProjectRepoClient;
 @Configuration
 public class RepoConfig {
 
-    @Value("${pride-repo.api.baseUrl}")
-    private String apiBaseUrl;
+    private final PrideRepoClientFactory prideRepoClientFactory;
 
-    @Value("${pride-repo.api.keyName}")
-    private String apiKeyName;
-
-    @Value("${pride-repo.api.keyValue}")
-    private String apiKeyValue;
-
-    @Autowired
-    private PrideRepoClientFactory prideRepoClientFactory;
+    public RepoConfig(@Value("${pride-repo.api.baseUrl}") String apiBaseUrl,
+                      @Value("${pride-repo.api.keyName}") String apiKeyName,
+                      @Value("${pride-repo.api.keyValue}") String apiKeyValue) {
+        this.prideRepoClientFactory = new PrideRepoClientFactory(apiBaseUrl, apiKeyName, apiKeyValue);
+    }
 
     @Bean
     public ProjectRepoClient getProjectRepoClient() {
         return prideRepoClientFactory.getProjectRepoClient();
-    }
-
-    @Bean
-    public PrideRepoClientFactory getPrideRepoClientFactory() {
-        return new PrideRepoClientFactory(apiBaseUrl, apiKeyName, apiKeyValue);
     }
 }
