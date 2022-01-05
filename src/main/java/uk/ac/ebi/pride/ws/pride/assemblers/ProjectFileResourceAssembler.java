81103ef01b7bded3b957812f16275e5faac73560
selvakumar kamatchinathan <selva.439@gmail.com>
2022-01-05 10:01:53 +0000

    2020ftpPath

--- src/main/java/uk/ac/ebi/pride/ws/pride/assemblers/ProjectFileResourceAssembler.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/assemblers/ProjectFileResourceAssembler.java
@@ -49,7 +49,9 @@ public class ProjectFileResourceAssembler extends ResourceAssemblerSupport<Mongo
                             value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2015/") ||
                             value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/") ||
                             value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2017/") ||
-                            value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2018/")
+                            value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2018/") ||
+                            value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2019/") ||
+                            value.startsWith("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2020/")
                     ) {
                         value = value.replace("ftp://ftp.pride.ebi.ac.uk/pride/data/archive/", "ftp://ftp.ebi.ac.uk/pride-archive/");
                     }
