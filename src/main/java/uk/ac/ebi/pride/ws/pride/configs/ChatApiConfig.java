41865a32f0af28aeecff88836a2541aff7d44fe0
selvakumar kamatchinathan <selva.439@gmail.com>
2023-10-02 23:34:05 +0100

    readTimeOut

--- src/main/java/uk/ac/ebi/pride/ws/pride/configs/ChatApiConfig.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/configs/ChatApiConfig.java
@@ -29,6 +29,7 @@ public class ChatApiConfig {
         if (proxyHost != null && proxyPort != null) {
             Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
             requestFactory.setProxy(proxy);
+            requestFactory.setReadTimeout(10000);
         }
         return new RestTemplate(requestFactory);
     }
