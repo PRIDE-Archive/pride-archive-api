c8dfcf47d382ca07ca747afd906792d8be54340c
Chakradhar Bandla <chakrabandla@gmail.com>
2024-07-18 20:18:03 +0100

    codec.maxInMemorySize=10MB

---
+++ src/main/java/uk/ac/ebi/pride/ws/pride/configs/WebFluxConfig.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.pride.ws.pride.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024); // 10MB
+    }
+}
