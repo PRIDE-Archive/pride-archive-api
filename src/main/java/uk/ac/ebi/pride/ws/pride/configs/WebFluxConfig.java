7ccf27a7f831200f053f2e5ffb19ae702bd5e42a
Chakradhar Bandla <chakrabandla@gmail.com>
2024-10-14 10:51:02 +0100

    CORS disable

--- src/main/java/uk/ac/ebi/pride/ws/pride/configs/WebFluxConfig.java
+++ src/main/java/uk/ac/ebi/pride/ws/pride/configs/WebFluxConfig.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.pride.ws.pride.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
@@ -10,5 +11,12 @@ public class WebFluxConfig implements WebFluxConfigurer {
     @Override
     public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
         configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024); // 10MB
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
